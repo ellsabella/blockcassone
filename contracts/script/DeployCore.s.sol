f95da162b0bd64d78498f9d5b8b1afc192e169db
Ella Whit <bibblebobble1110@gmail.com>
2026-05-28 07:48:06 +0100

    Add core deployment script

---
+++ contracts/script/DeployCore.s.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {CubeMintController} from "../src/CubeMintController.sol";
+import {CubeNFT} from "../src/CubeNFT.sol";
+import {FlatteningAttestation} from "../src/FlatteningAttestation.sol";
+import {NonNormieArtStore} from "../src/NonNormieArtStore.sol";
+import {NormieAddresses} from "../src/NormieAddresses.sol";
+
+contract DeployCore is Script {
+    struct Deployment {
+        CubeNFT cubes;
+        NonNormieArtStore artStore;
+        FlatteningAttestation attestation;
+        CubeMintController controller;
+    }
+
+    uint32 internal constant DEFAULT_TOTAL_SLOTS = 4096;
+
+    function run() external returns (Deployment memory deployment) {
+        address initialOwner = vm.envOr("BLOCKCASSONE_OWNER", msg.sender);
+        address attestationSigner = vm.envOr("BLOCKCASSONE_ATTESTATION_SIGNER", initialOwner);
+        uint32 totalSlots = uint32(vm.envOr("BLOCKCASSONE_TOTAL_SLOTS", uint256(DEFAULT_TOTAL_SLOTS)));
+
+        vm.startBroadcast();
+        deployment = _deploy(initialOwner, attestationSigner, NormieAddresses.NORMIES, totalSlots);
+        vm.stopBroadcast();
+
+        _logDeployment(deployment);
+    }
+
+    function _deploy(
+        address initialOwner,
+        address attestationSigner,
+        address normies,
+        uint32 totalSlots
+    ) internal returns (Deployment memory deployment) {
+        deployment.cubes = new CubeNFT(
+            "Blockcassone Cubes",
+            "CUBE",
+            normies,
+            totalSlots,
+            initialOwner
+        );
+        deployment.artStore = new NonNormieArtStore(initialOwner);
+        deployment.attestation = new FlatteningAttestation(initialOwner, attestationSigner);
+        deployment.controller =
+            new CubeMintController(deployment.cubes, deployment.artStore, deployment.attestation);
+
+        deployment.cubes.transferOwnership(address(deployment.controller));
+        deployment.artStore.transferOwnership(address(deployment.controller));
+        deployment.attestation.setAuthorizedConsumer(address(deployment.controller));
+    }
+
+    function _logDeployment(Deployment memory deployment) internal view {
+        console2.log("CubeNFT", address(deployment.cubes));
+        console2.log("NonNormieArtStore", address(deployment.artStore));
+        console2.log("FlatteningAttestation", address(deployment.attestation));
+        console2.log("CubeMintController", address(deployment.controller));
+        console2.log("CubeNFT owner", deployment.cubes.owner());
+        console2.log("ArtStore owner", deployment.artStore.owner());
+        console2.log("Authorized consumer", deployment.attestation.authorizedConsumer());
+    }
+}
---
+++ contracts/test/DeployCore.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {CubeMintController} from "../src/CubeMintController.sol";
+import {DeployCore} from "../script/DeployCore.s.sol";
+import {FlatteningAttestation} from "../src/FlatteningAttestation.sol";
+import {NonNormieArtStore} from "../src/NonNormieArtStore.sol";
+
+contract DeployCoreHarness is DeployCore {
+    function deploy(
+        address initialOwner,
+        address attestationSigner,
+        address normies,
+        uint32 totalSlots
+    ) external returns (Deployment memory deployment) {
+        return _deploy(initialOwner, attestationSigner, normies, totalSlots);
+    }
+}
+
+contract DeployCoreTest is Test {
+    address private constant OWNER = address(0xA11CE);
+    address private constant SIGNER = address(0xB0B);
+
+    MockNormiesForDeploy private normies;
+    DeployCoreHarness private deployer;
+
+    function setUp() public {
+        normies = new MockNormiesForDeploy();
+        deployer = new DeployCoreHarness();
+    }
+
+    function testDeploysAndWiresCoreSystem() public {
+        DeployCore.Deployment memory deployment =
+            deployer.deploy(OWNER, SIGNER, address(normies), 4096);
+
+        assertEq(deployment.cubes.normieContract(), address(normies));
+        assertEq(deployment.cubes.totalSlots(), 4096);
+        assertEq(deployment.cubes.owner(), address(deployment.controller));
+        assertEq(deployment.artStore.owner(), address(deployment.controller));
+        assertEq(deployment.attestation.owner(), OWNER);
+        assertEq(deployment.attestation.attestationSigner(), SIGNER);
+        assertEq(deployment.attestation.authorizedConsumer(), address(deployment.controller));
+
+        assertEq(address(deployment.controller.cubes()), address(deployment.cubes));
+        assertEq(address(deployment.controller.artStore()), address(deployment.artStore));
+        assertEq(
+            address(deployment.controller.attestationVerifier()),
+            address(deployment.attestation)
+        );
+    }
+}
+
+contract MockNormiesForDeploy {
+    function ownerOf(uint256) external pure returns (address) {
+        return address(0);
+    }
+}
