1baf884d546693b2ce03a4659b1e935c8ac33dc3
Ella Whit <bibblebobble1110@gmail.com>
2026-05-28 07:25:34 +0100

    Fix Normie address checksums

--- contracts/src/NormieAddresses.sol
+++ contracts/src/NormieAddresses.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.26;
 
 library NormieAddresses {
-    address public constant NORMIES = 0x9eb6e2025b64f340691e424b7fe7022ffde12438;
-    address public constant NORMIES_STORAGE = 0x1b976baf51cf51f0e369c070d47fbc47a706e602;
-    address public constant NORMIES_RENDERER = 0xbe57fc4d0c729b8e8d33b638dd441f57365e4c25;
-    address public constant NORMIES_RENDERER_V2 = 0x7818f24d3239c945510e0a1a523dd9971812c6c0;
-    address public constant NORMIES_RENDERER_V3 = 0x1af01b902256d77cf9499a14ef4e494897380b05;
-    address public constant NORMIES_RENDERER_V4 = 0x8ec46cc1f306652868a4dfbaaae87cba2715a0eb;
-    address public constant NORMIES_MINTER = 0xc74994dd70ffb621cc514ce18a4f6f52124e296d;
-    address public constant NORMIES_MINTER_V2 = 0xc513272597d3022d77b3d7eeba92cea5d7fb2808;
-    address public constant NORMIES_CANVAS = 0x64951d92e345c50381267380e2975f66810e869c;
-    address public constant NORMIES_CANVAS_STORAGE = 0xc255be0983776bab027a156681b6925cde47b2d1;
+    address public constant NORMIES = 0x9Eb6E2025B64f340691e424b7fe7022fFDE12438;
+    address public constant NORMIES_STORAGE = 0x1B976bAf51cF51F0e369C070d47FBc47A706e602;
+    address public constant NORMIES_RENDERER = 0xBe57fC4D0c729b8e8d33b638Dd441F57365e4c25;
+    address public constant NORMIES_RENDERER_V2 = 0x7818f24D3239C945510e0a1A523dd9971812C6C0;
+    address public constant NORMIES_RENDERER_V3 = 0x1Af01b902256D77cf9499A14Ef4E494897380B05;
+    address public constant NORMIES_RENDERER_V4 = 0x8eC46Cc1f306652868a4dfbAAae87CBa2715A0eB;
+    address public constant NORMIES_MINTER = 0xC74994dD70FFb621CC514cE18a4F6F52124e296d;
+    address public constant NORMIES_MINTER_V2 = 0xc513272597d3022D77b3d7EEBA92cea5D7fb2808;
+    address public constant NORMIES_CANVAS = 0x64951d92e345C50381267380e2975f66810E869c;
+    address public constant NORMIES_CANVAS_STORAGE = 0xC255BE0983776BAB027a156681b6925cde47B2D1;
 
     struct Config {
         address normies;
